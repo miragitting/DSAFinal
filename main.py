39779d4a6b666f56e5cb1cb76182fb4c308182a4
GeorgeMacBook <Mintymilk1@gmail.com>
2023-05-16 13:02:39 -0600

    main.py skeleton

---
+++ main.py
@@ -0,0 +1,19 @@
+class tile():
+    
+    def __init__(self, x ,y, passable):
+        self.xint = x
+        self.yint = y
+        self.passablebool = passable
+
+class entrance():
+    def __init__(self, border ,tiles):
+        
+        self.tilesarray = tiles 
+        self.borderval = border 
+
+class abstractTile():
+    
+    def __init__(self, tiles, entrances):
+
+        self.tilesarry= tiles 
+        self.entrancesarray = entrances
