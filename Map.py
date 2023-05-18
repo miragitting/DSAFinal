0a7b7d1eed9e09e09f5d9f6b73e1859d88dc5c0d
GeorgeMacBook <Mintymilk1@gmail.com>
2023-05-18 11:08:11 -0600

    Map.py added

---
+++ Map.py
@@ -0,0 +1,12 @@
+import networkx as nx
+class Map():
+    #height, width, and have a remove function that removes a vertex
+    def __init__(self, height, width):
+        
+        self.gr = nx.grid_graph((height, width))
+        
+    def removeNode():
+        self.gr.remove_node()
+        
+#    map = Map(1,5)
+
